3a64bd5651ff41c0012a86292e3cc9521de303b7
James Connolly <jimmy@1024bees.com>
2022-03-24 23:34:37 -0400

    through the end of 3.3

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
-use axum::{extract, response::IntoResponse, routing::get, Router};
+use axum::{
+    extract,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
 
 use std::net::SocketAddr;
-async fn greeting(extract::Path(name): extract::Path<String>) -> String {
-    format!("Hello {}", name)
-}
 
-async fn basic_greeting() -> String {
-    format!("Hello World")
+async fn health_check() -> impl IntoResponse {
+    StatusCode::OK
 }
 
 #[tokio::main]
 async fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    let app = Router::new()
-        .route("/", get(basic_greeting))
-        .route("/:name", get(greeting));
+    let app = Router::new().route("/health_check", get(health_check));
+
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
