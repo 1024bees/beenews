538162b28feb3c27d28bc7c4b9f06c2a2ae2a1f4
James Connolly <jimmy@1024bees.com>
2022-03-27 13:35:32 -0400

    completed through 3.7.3.1. One difference between axum and actix is that a parsing error (e.g. Deserialization of a form fails) returns a 422 error code in axum
    This seems to be fine, and more correct than a 400 return code; it is
    emitted because the form could be parsed but there are missing fields.
    
    Something to check with the axum folks is if an html form is malformed
    (meta questiwhat does that look like?) is the correct error emitted? my guess would
    be a 400 would be most apropriate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use axum::extract::Form;
+use axum::routing::post;
 use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
 
 use std::future::Future;
@@ -7,8 +9,21 @@ async fn health_check() -> impl IntoResponse {
     StatusCode::OK
 }
 
+#[derive(serde::Deserialize, Debug)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(Form(form): Form<FormData>) -> impl IntoResponse {
+    println!("form is {:#?}", form);
+    StatusCode::OK
+}
+
 pub fn run(listener: TcpListener) -> impl Future<Output = Result<(), hyper::Error>> {
-    let app = Router::new().route("/health_check", get(health_check));
+    let app = Router::new()
+        .route("/health_check", get(health_check))
+        .route("/subscriptions", post(subscribe));
     axum::Server::from_tcp(listener)
         .expect("Spawning server from listener failed")
         .serve(app.into_make_service())
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,7 @@
-use axum::{
-    extract,
-    http::StatusCode,
-    response::{IntoResponse, Response},
-    routing::get,
-    Router,
-};
-
-use std::net::SocketAddr;
-
-async fn health_check() -> impl IntoResponse {
-    StatusCode::OK
-}
+use zero2bees::run;
 
 #[tokio::main]
 async fn main() {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    let app = Router::new().route("/health_check", get(health_check));
-
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .expect("Honestly no idea what the expectation is here");
+    let listener =std::net::TcpListener::bind("127.0.0.1:3000").expect("could not bind to port 3000");
+    run(listener).await.expect("Starting app failed");
 }
