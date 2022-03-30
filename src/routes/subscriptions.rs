639cb8ab738bcc503894355237eb11da6f2b55a5
James Connolly <jimmy@1024bees.com>
2022-03-29 23:49:13 -0400

    through 4.5.8.. woops, didn't commit as much as I'd like

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -5,6 +5,7 @@ use axum::extract::{Extension, Form};
 use axum::{http::StatusCode, response::IntoResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize, Debug)]
@@ -17,11 +18,20 @@ pub async fn subscribe(
     Form(form): Form<FormData>,
     Extension(connection): Extension<Arc<PgPool>>,
 ) -> impl IntoResponse {
+    let request_id = Uuid::new_v4();
+    // Spans, like logs, have an associated level
+    // `info_span` creates a span at the info-level
+    let request_span = tracing::info_span!(
+    "Adding a new subscriber WOOHOO! ." ,
+        % request_id ,
+        subscriber_email = % form . email ,
+        subscriber_name = % form . name
+    );
+    let _request_span_guard = request_span.enter();
+    let query_span = tracing::info_span!("Saving new subscriber details in the database",);
+
     match sqlx::query!(
-        r#"
-INSERT INTO subscriptions (id, email, name, subscribed_at)
-VALUES ($1, $2, $3, $4)
-"#,
+        r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
         Uuid::new_v4(),
         form.email,
         form.name,
@@ -30,11 +40,15 @@ VALUES ($1, $2, $3, $4)
     // We use `get_ref` to get an immutable reference to the `PgConnection`
     // wrapped by `web::Data`.
     .execute(connection.as_ref())
+    .instrument(query_span)
     .await
     {
-        Ok(_) => StatusCode::OK,
+        Ok(_) => {
+            tracing::info!("req_id {}: Successfully added user details!", request_id);
+            StatusCode::OK
+        }
         Err(e) => {
-            log::warn!("Failed to execute query: {}", e);
+            tracing::error!("req_id {}: Failed to execute query: {:?}", request_id, e);
             StatusCode::INTERNAL_SERVER_ERROR
         }
     }
--- src/startup.rs
+++ src/startup.rs
@@ -4,22 +4,45 @@ use axum::{
     Router,
 };
 
-use super::routes::health_check;
-use super::routes::subscribe;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
-use std::future::Future;
-use std::sync::Arc;
+use super::routes::{health_check, subscribe};
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 
 use sqlx::PgPool;
+use std::future::Future;
 use std::net::TcpListener;
+use std::sync::Arc;
 pub fn run(
     listener: TcpListener,
     connection: PgPool,
 ) -> impl Future<Output = Result<(), hyper::Error>> {
+    //env_logger::Builder::from_env(Env:: default () . default_filter_or("info")) . init() ;
+    //
+    //
+    //
+    //
+    let formatting_layer = BunyanFormattingLayer::new(
+        "zero2prod".into(),
+        // Output the formatted spans to stdout.
+        std::io::stdout,
+    );
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| EnvFilter::new("info")),
+        )
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
+        .init();
+
     let app = Router::new()
         .route("/health_check", get(health_check))
         .route("/subscriptions", post(subscribe))
+        .layer(TraceLayer::new_for_http())
         .layer(Extension(Arc::new(connection)));
+
     axum::Server::from_tcp(listener)
         .expect("Spawning server from listener failed")
         .serve(app.into_make_service())
