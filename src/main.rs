57f2e03c431a39490fca384c13f771e3f0f56cb1
James Connolly <jimmy@1024bees.com>
2022-03-30 00:03:53 -0400

    through 4.5.11

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use sqlx::PgPool;
 use zero2bees::configuration::get_configuration;
 use zero2bees::startup::run;
+use zero2bees::telemetry::{get_subscriber,init_subscriber};
 #[tokio::main]
 async fn main() {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection = PgPool::connect(&configuration.database.connection_string())
         .await
--- src/startup.rs
+++ src/startup.rs
@@ -5,10 +5,8 @@ use axum::{
 };
 
 use tower_http::trace::TraceLayer;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
-
 use super::routes::{health_check, subscribe};
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+
 
 use sqlx::PgPool;
 use std::future::Future;
@@ -23,20 +21,7 @@ pub fn run(
     //
     //
     //
-    let formatting_layer = BunyanFormattingLayer::new(
-        "zero2prod".into(),
-        // Output the formatted spans to stdout.
-        std::io::stdout,
-    );
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| EnvFilter::new("info")),
-        )
-        .with(JsonStorageLayer)
-        .with(formatting_layer)
-        .init();
-
+    
     let app = Router::new()
         .route("/health_check", get(health_check))
         .route("/subscriptions", post(subscribe))
