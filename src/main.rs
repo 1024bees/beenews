46195c84d160e97399220f6966572a190def5240
James Connolly <jimmy@1024bees.com>
2022-05-01 09:26:54 -0400

    up to 7.3.9

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,17 @@
 use sqlx::postgres::PgPoolOptions;
 use zero2bees::configuration::get_configuration;
 use zero2bees::email_client::EmailClient;
-use zero2bees::startup::run;
+use zero2bees::startup::{run, Application};
 use zero2bees::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), hyper::Error> {
     let subscriber = get_subscriber("zero2bees".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
-
-    let connection = PgPoolOptions::new()
-        .connect_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(configuration.database.with_db());
-
-    // Build an `EmailClient` using `configuration`
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    let config = get_configuration().expect("Failed to read configuration");
-    let addr = format!("{}:{}", config.application.host, config.application.port);
-    tracing::info!("starting app at {}", addr);
-    let listener = std::net::TcpListener::bind(addr).expect("could not bind addr");
-    run(listener, connection, email_client)
+    let app = Application::build(configuration)
         .await
-        .expect("Starting app failed");
+        .expect("App build failed");
+    app.run_until_stopped().await?;
+    Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,26 +1,55 @@
+use super::routes::{health_check, subscribe};
+use crate::email_client::EmailClient;
+use axum::body::Body;
+use axum::http::Request;
 use axum::{
     extract::Extension,
     routing::{get, post},
     Router,
 };
-
-use crate::email_client::EmailClient;
-
-use super::routes::{health_check, subscribe};
-use tower_http::trace::TraceLayer;
-
-use tower_request_id::{RequestId, RequestIdLayer};
-
-use axum::body::Body;
-use axum::http::Request;
 use sqlx::PgPool;
 use std::future::Future;
 use std::net::TcpListener;
+use std::pin::Pin;
 use std::sync::Arc;
+use tower_http::trace::TraceLayer;
+use tower_request_id::{RequestId, RequestIdLayer};
+
+use crate::configuration::{get_configuration, DatabaseSettings, Settings};
+use sqlx::postgres::PgPoolOptions;
+
+pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
+    PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.with_db())
+}
+
+pub async fn build(configuration: Settings) -> impl Future<Output = Result<(), hyper::Error>> {
+    let connection = get_connection_pool(&configuration.database);
+    // Build an `EmailClient` using `configuration`
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+    let timeout = configuration.email_client.timeout();
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout,
+    );
+
+    let config = get_configuration().expect("Failed to read configuration");
+    let addr = format!("{}:{}", config.application.host, config.application.port);
+    tracing::info!("starting app at {}", addr);
+    let listener = std::net::TcpListener::bind(addr).expect("could not bind addr");
+    run(listener, connection, email_client)
+}
+
 pub fn run(
     listener: TcpListener,
     connection: PgPool,
-    email_client: EmailClient
+    email_client: EmailClient,
 ) -> impl Future<Output = Result<(), hyper::Error>> {
     let app = Router::new()
         .route("/health_check", get(health_check))
@@ -52,4 +81,52 @@ pub fn run(
     axum::Server::from_tcp(listener)
         .expect("Spawning server from listener failed")
         .serve(app.into_make_service())
+}
+
+// A new type to hold the newly built server and its port
+pub struct Application {
+    port: u16,
+    listener: TcpListener,
+    connection_pool: PgPool,
+    email_client: EmailClient,
+}
+impl Application {
+    // We have converted the `build` function into a constructor for
+    // `Application`.
+    pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
+        let connection_pool = get_connection_pool(&configuration.database);
+        let sender_email = configuration
+            .email_client
+            .sender()
+            .expect("Invalid sender email address.");
+        let timeout = configuration.email_client.timeout();
+        let email_client = EmailClient::new(
+            configuration.email_client.base_url,
+            sender_email,
+            configuration.email_client.authorization_token,
+            timeout,
+        );
+        let address = format!(
+            "{}:{}",
+            configuration.application.host, configuration.application.port
+        );
+        let listener = TcpListener::bind(&address)?;
+        let port = listener.local_addr().unwrap().port();
+
+        // We "save" the bound port in one of `Application`'s fields
+        Ok(Self {
+            listener,
+            connection_pool,
+            email_client,
+            port,
+        })
+    }
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+    // A more expressive name that makes it clear that
+    // this function only returns when the application is stopped.
+    pub async fn run_until_stopped(self) -> Result<(), hyper::Error> {
+        run(self.listener, self.connection_pool, self.email_client).await
+    }
 }
